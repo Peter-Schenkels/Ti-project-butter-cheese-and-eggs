8396f38875b89c3b952e622bc82a46500f3e41c9
Peter Schenkels <43245077+Peter-Schenkels@users.noreply.github.com>
2019-04-04 23:26:11 +0200

    Update arlin.cpp

--- arlin.cpp
+++ arlin.cpp
@@ -6,29 +6,30 @@ int move(bool beurt, int size, vector<int> & speelveldt, int diepte, vector<int>
 	vector<int> speelveld = speelveldt;
 	//0 is beurt voor computer, 1 is beurt voor de speler
 	if(beurt == 0){
-		cout << "\naan de beurt Computer\n";
+//		cout << "\naan de beurt Computer\n";
 		for(unsigned int i =0; i < 9; i++){
 			if (speelveld[i] == 0){
 				speelveld[i] = 2;
 				
-				cout << "\nhuidige diepte: " << diepte << "\n";
-				for (unsigned int j=0; j < 9; j++){
-					if(j%3 == 0){
-						cout << "\n";
-					}
-					cout << speelveld[j];
-				}
-				cout << "\n";
+//				cout << "\nhuidige diepte: " << diepte << "\n";
+//				for (unsigned int j=0; j < 9; j++){
+//					if(j%3 == 0){
+//						cout << "\n";
+//					}
+//					cout << speelveld[j];
+//				}
+//				cout << "\n";
+				
 				if (speelveld[0] == 2 && speelveld[1] == 2 && speelveld[2] == 2){diepteEen.push_back(diepte); return 1;}
 				if (speelveld[3] == 2 && speelveld[4] == 2 && speelveld[5] == 2){diepteEen.push_back(diepte); return 1;}
 				if (speelveld[6] == 2 && speelveld[7] == 2 && speelveld[8] == 2){diepteEen.push_back(diepte); return 1;}
 				
-				if (speelveld[0] == 2 && speelveld[4] == 2 && speelveld[8] == 2){diepteEen.push_back(diepte);  return 1;}
-				if (speelveld[6] == 2 && speelveld[4] == 2 && speelveld[2] == 2){diepteEen.push_back(diepte);  return 1;}
+				if (speelveld[0] == 2 && speelveld[4] == 2 && speelveld[8] == 2){diepteEen.push_back(diepte); return 1;}
+				if (speelveld[6] == 2 && speelveld[4] == 2 && speelveld[2] == 2){diepteEen.push_back(diepte); return 1;}
 				
-				if (speelveld[0] == 2 && speelveld[3] == 2 && speelveld[6] == 2){diepteEen.push_back(diepte);  return 1;}
-				if (speelveld[1] == 2 && speelveld[4] == 2 && speelveld[7] == 2){diepteEen.push_back(diepte);  return 1;}
-				if (speelveld[2] == 2 && speelveld[5] == 2 && speelveld[8] == 2){diepteEen.push_back(diepte);  return 1;}
+				if (speelveld[0] == 2 && speelveld[3] == 2 && speelveld[6] == 2){diepteEen.push_back(diepte); return 1;}
+				if (speelveld[1] == 2 && speelveld[4] == 2 && speelveld[7] == 2){diepteEen.push_back(diepte); return 1;}
+				if (speelveld[2] == 2 && speelveld[5] == 2 && speelveld[8] == 2){diepteEen.push_back(diepte); return 1;}
 				
 				
 				//Als speel veld vol is
@@ -38,41 +39,45 @@ int move(bool beurt, int size, vector<int> & speelveldt, int diepte, vector<int>
 	}
 	//speler
 	if(beurt == 1){
-		cout << "\naan de beurt Speler\n";
+		//cout << "\naan de beurt Speler\n";
 		for(unsigned int i =0; i < 9; i++){
 			if(speelveld[i] == 0){
 				speelveld[i] = 1;
 			
-			cout << "\nhuidige diepte: " << diepte << "\n";
-			for (unsigned int j=0; j < 9; j++){
-					if(j%3 == 0){
-						cout << "\n";
-					}
-					cout << speelveld[j];
-				}
-			cout << "\n";
-			if (speelveld[0] == 1 && speelveld[1] == 1 && speelveld[2] == 1){diepteNul.push_back(diepte);return 0;}
-			if (speelveld[3] == 1 && speelveld[4] == 1 && speelveld[5] == 1){diepteNul.push_back(diepte);return 0;}
-			if (speelveld[6] == 1 && speelveld[7] == 1 && speelveld[8] == 1){diepteNul.push_back(diepte);return 0;}
+//			cout << "\nhuidige diepte: " << diepte << "\n";
+//			for (unsigned int j=0; j < 9; j++){
+//					if(j%3 == 0){
+//						cout << "\n";
+//					}
+//					cout << speelveld[j];
+//				}
+//			cout << "\n";
+			
+			if (speelveld[0] == 1 && speelveld[1] == 1 && speelveld[2] == 1){diepteNul.push_back(diepte); return 0;}
+			if (speelveld[3] == 1 && speelveld[4] == 1 && speelveld[5] == 1){diepteNul.push_back(diepte); return 0;}
+			if (speelveld[6] == 1 && speelveld[7] == 1 && speelveld[8] == 1){diepteNul.push_back(diepte); return 0;}
 				
-			if (speelveld[0] == 1 && speelveld[4] == 1 && speelveld[8] == 1){diepteNul.push_back(diepte);return 0;}
-			if (speelveld[6] == 1 && speelveld[4] == 1 && speelveld[2] == 1){diepteNul.push_back(diepte);return 0;}
+			if (speelveld[0] == 1 && speelveld[4] == 1 && speelveld[8] == 1){diepteNul.push_back(diepte); return 0;}
+			if (speelveld[6] == 1 && speelveld[4] == 1 && speelveld[2] == 1){diepteNul.push_back(diepte); return 0;}
 
 			if (speelveld[0] == 1 && speelveld[3] == 1 && speelveld[6] == 1){diepteNul.push_back(diepte); return 0;}
 			if (speelveld[1] == 1 && speelveld[4] == 1 && speelveld[7] == 1){diepteNul.push_back(diepte); return 0;}
 			if (speelveld[2] == 1 && speelveld[5] == 1 && speelveld[8] == 1){diepteNul.push_back(diepte); return 0;}
+			
 			move(false, size-1, speelveld, diepte+1, diepteNul, diepteEen);
 			}
 		}
 	}
 	if(size == 0){
-		diepteEen.push_back(1000); return 1; 
+		cout << "hey code is limit";
+		return 0;
 	}
 	
 }
 
 
 int main(){
+	int firstmove = 0;
 	int tmpVerschil = 0;
 	vector<vector<int>> mogelijkheden = {};
 	vector<int> diepteNul = {};
@@ -81,9 +86,9 @@ int main(){
 	int tijdelijkeGrootsteVerschil = 0;
 	int indexTijdelijkeGrootsteVerschil = 0;
 	int besteZet = -1;
-	vector<int> speelveldje = {1,2,2,
-							   0,0,2,
-							   1,0,1};
+	vector<int> speelveldje = {1S,0,0,
+							   0,0,0,
+							   0,0,0};
 	
 	//berekent aantal vrije plekken
 	for(unsigned int i = 0; i < speelveldje.size(); i++){
@@ -93,21 +98,37 @@ int main(){
 	}
 	
 	for(unsigned int i = 0; i < speelveldje.size(); i++){
-		cout << "\n"<< i << "\n";
 		if (speelveldje[i] == 0){
 			diepteEen.clear();
 			diepteNul.clear();
+			firstmove = 0;
 			
 
 			
 			vector<int> tmp = speelveldje;
 			
 			tmp[i] = 2;
+			if (tmp[0] == 2 && tmp[1] == 2 && tmp[2] == 2){besteZet = i; firstmove = 1;}
+			if (tmp[3] == 2 && tmp[4] == 2 && tmp[5] == 2){besteZet = i; firstmove = 1;}
+			if (tmp[6] == 2 && tmp[7] == 2 && tmp[8] == 2){besteZet = i; firstmove = 1;}
+				
+			if (tmp[0] == 2 && tmp[4] == 2 && tmp[8] == 2){besteZet = i; firstmove = 1;}
+			if (tmp[6] == 2 && tmp[4] == 2 && tmp[2] == 2){besteZet = i; firstmove = 1;}
+				
+			if (tmp[0] == 2 && tmp[3] == 2 && tmp[6] == 2){besteZet = i; firstmove = 1;}
+			if (tmp[1] == 2 && tmp[4] == 2 && tmp[7] == 2){besteZet = i; firstmove = 1;}
+			if (tmp[2] == 2 && tmp[5] == 2 && tmp[8] == 2){besteZet = i; firstmove = 1;}
+			
 
+			
+			if (firstmove == 1){
+			
+			break;
+			}
+			
+			
+			cout << "gay";
 			move(true, aantalVrijePlekken, tmp, 0, diepteNul, diepteEen);
-
-			
-			
 			//min diepte berekenen van een
 			int tmpDiepte = diepteEen[0];
 			for (unsigned int j=1; j < diepteEen.size(); j++){
@@ -140,6 +161,8 @@ int main(){
 				mogelijkheden.push_back({i, tmpVerschil});
 				
 			}
+				
+			
 			
 			
 			
@@ -153,19 +176,23 @@ int main(){
 		}
 		
 	}
-	int max = mogelijkheden[0][1];
-	int maxIndex = mogelijkheden[0][0];
-	for(unsigned int i = 1; i < mogelijkheden.size(); i++){
-		
-		if (max < mogelijkheden[i][1]){
+	if(firstmove == 0){
+		int max = mogelijkheden[0][1];
+		int maxIndex = mogelijkheden[0][0];
+		for(unsigned int i = 1; i < mogelijkheden.size(); i++){
 			
-			max = mogelijkheden[i][1];
-			maxIndex = mogelijkheden[i][0];
+			if (max < mogelijkheden[i][1]){
+				
+				max = mogelijkheden[i][1];
+				maxIndex = mogelijkheden[i][0];
+				
+			}
 			
 		}
+		besteZet = maxIndex;
 		
 	}
-	besteZet = maxIndex;
+		
 	cout << "\nDe index daarvan is: " << besteZet << "\n";
 	
 	
