f7de163b8eec75f3820e0d423503a515eb097a89
Peter Schenkels <43245077+Peter-Schenkels@users.noreply.github.com>
2019-04-04 19:24:19 +0200

    Update arlin.cpp

--- arlin.cpp
+++ arlin.cpp
@@ -9,7 +9,7 @@ int move(bool beurt, int size, vector<int> & speelveldt, int pointer, int diepte
 		for(unsigned int i =pointer; i < 9; i++){
 			
 			speelveld[i] = 1;
-			if (speelveld[0] == 1 && speelveld[1] == 1 && speelveld[2] == 1){diepteNul.push_back(diepte);  return 0;}
+			if (speelveld[0] == 1 && speelveld[1] == 1 && speelveld[2] == 1){diepteNul.push_back(diepte); return 0;}
 			if (speelveld[3] == 1 && speelveld[4] == 1 && speelveld[5] == 1){diepteNul.push_back(diepte); return 0;}
 			if (speelveld[6] == 1 && speelveld[7] == 1 && speelveld[8] == 1){diepteNul.push_back(diepte); return 0;}
 				
@@ -19,7 +19,7 @@ int move(bool beurt, int size, vector<int> & speelveldt, int pointer, int diepte
 			if (speelveld[0] == 1 && speelveld[3] == 1 && speelveld[6] == 1){diepteNul.push_back(diepte); return 0;}
 			if (speelveld[1] == 1 && speelveld[4] == 1 && speelveld[7] == 1){diepteNul.push_back(diepte); return 0;}
 			if (speelveld[2] == 1 && speelveld[5] == 1 && speelveld[8] == 1){diepteNul.push_back(diepte); return 0;}
-			move(1, size-1, speelveld, pointer+1, diepte+1, diepteNul, diepteEen);
+			move(true, size-1, speelveld, pointer+1, diepte+1, diepteNul, diepteEen);
 			
 		}
 		
@@ -27,8 +27,6 @@ int move(bool beurt, int size, vector<int> & speelveldt, int pointer, int diepte
 	if(beurt == 1){
 		for(unsigned int i =pointer; i < 9; i++){
 			if (speelveld[i] == 0){
-				
-				speelveld[i] = 2;
 				speelveld[i] = 2;
 				
 				if (speelveld[0] == 2 && speelveld[1] == 2 && speelveld[2] == 2){diepteEen.push_back(diepte); return 1;}
@@ -46,17 +44,17 @@ int move(bool beurt, int size, vector<int> & speelveldt, int pointer, int diepte
 				
 				
 				//Als speel veld vol is
-				move(0, size-1, speelveld, pointer+1, diepte+1, diepteNul, diepteEen);
+				move(false, size-1, speelveld, pointer+1, diepte+1, diepteNul, diepteEen);
 					
-				
-				if(size == 0){
-					
-					return 1;
 				}
+			if(size == 0){
+				
 				
 			}
+				
 			
-		else{continue;}
+			
+			else{continue;}
 					
 					
 					
@@ -76,8 +74,8 @@ int move(bool beurt, int size, vector<int> & speelveldt, int pointer, int diepte
 int main(){
 	float nulletjes = 0;
 	float eentjes = 0;
-	vector<int> diepteNul;
-	vector<int> diepteEen;
+	vector<int> diepteNul {};
+	vector<int> diepteEen {};
 	int aantalVrijePlekken = 0;
 	int tijdelijkeGrootsteVerschil = 0;
 	int indexTijdelijkeGrootsteVerschil = 0;
@@ -85,13 +83,7 @@ int main(){
 	vector<int> lol = {1,1,0,
 					   0,0,2,
 					   2,0,0};
-	for (unsigned int i=0; i < 9; i++){
-		if(i%3 == 0){
-			cout << "\n";
-			}
-		cout << lol[i];
-	}
-	cout << "\nonnaangepast\n";
+	
 	
 	for(int i = 0; i < lol.size(); i++){
 		if(lol[i] == 0){
@@ -106,14 +98,26 @@ int main(){
 			cout << "\n";
 			vector<int> tmp = lol;
 			tmp[i] = 2;
+
 			move(false, aantalVrijePlekken, tmp, 0, 0, diepteNul, diepteEen);
 			//cout << eentjes << " :eentjes\n" << nulletjes << " :nulletjes\n";
 			
-			//max van alle dieptes berekenen
+			//min van alle dieptes berekenen
+			for (unsigned int i=1; i < diepteNul.size(); i++){
+				cout << diepteNul[i] << ": van 0\n";
+			}
+			for (unsigned int i=1; i < diepteEen.size(); i++){
+				cout << diepteEen[i] << ": van 1\n";
+			}
+				
 			int tmpDiepte = diepteNul[0];
 			for (unsigned int i=1; i < diepteNul.size(); i++){
-				
-				if (tmpDiepte < diepteNul[i]){
+				//cout << "0: " << diepteNul[i] << "\n";
+				if (diepteNul[i] == 0){
+					
+					continue;
+				}
+				if (tmpDiepte > diepteNul[i]){
 					
 					tmpDiepte = diepteNul[i];
 				}
@@ -121,8 +125,12 @@ int main(){
 			cout << "Van nul: "<<tmpDiepte << "\n";
 			tmpDiepte = diepteEen[0];
 			for (unsigned int i=1; i < diepteEen.size(); i++){
-				
-				if (tmpDiepte < diepteEen[i]){
+				//cout << "1:" << diepteEen[i] << "\n";
+				if (diepteEen[i] == 0){
+					
+					continue;
+				}
+				if (tmpDiepte > diepteEen[i]){
 					
 					tmpDiepte = diepteEen[i];
 				}
