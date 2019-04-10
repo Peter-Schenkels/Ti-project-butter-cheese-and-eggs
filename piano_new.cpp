64ebd14143b4aa890b2157a44921d024e60ebab2
jellehu <43246552+jellehu@users.noreply.github.com>
2019-04-10 15:29:01 +0200

    Update piano_new.cpp

--- piano_new.cpp
+++ piano_new.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
-#include <Windows.H>
 #include <vector>
 #include "BrickPi3.cpp"
+#include <stdio.h>      // for printf
+#include <unistd.h>     // for usleep
+#include <signal.h>     // for catching exit signals
+#include <time.h> 
 using namespace std;
 
+BrickPi3 BP;
+void exit_signal_handler(int signo);
 
-//convert de positie naar afstand vergeleken met de vorige
+// Convert de positie naar afstand vergeleken met de vorige
 vector<int> piano(const vector<int> & noots){
 	
 	vector<int> tweedeList = {};
-	tweedeList.push_back(noots[0]);
+	tweedeList.push_back(noots[0]-1);
 	for(int i = 0; i < noots.size()-1; i++){
 		tweedeList.push_back(noots[i+1]-noots[i]);
 	}
 	return tweedeList;
 }
 
-//convert noten om in posities op het keyboard
+// Convert noten om in posities op het keyboard
 vector<int> transform(vector<string> & noots){
+	
 	vector<int> newList = {};
 	
 	for(int i =0; i < noots.size(); i++){
-		
 		if (noots[i] == "c"){
 			newList.push_back(1);
 		} else if (noots[i] == "d"){
@@ -53,47 +58,70 @@ vector<int> transform(vector<string> & noots){
 		} else if (noots[i] == "c16"){
 			newList.push_back(15);
 		} else {
-			cout << "Deze noot bestaat niet!";
+			printf("Deze noot bestaat niet!");
 		}
 	}
+	
 	return piano(newList);
 }
 
-// geef de lijst mee van de posities en zorg dat robot alle toetsen indrukt
+// Geef de lijst mee van de posities en zorg dat robot alle toetsen indrukt
 void move(vector<int> & stappen){
+	BP.set_motor_position(PORT_A, -200);
+	sleep(1);
+	BP.set_motor_position(PORT_A, 0);
+
+
 	for (unsigned int i = 0; i < stappen.size(); i++) {
-		if (i < 0){
-			BP.set_motor_dps(PORT_C, -50);
-			BP.set_motor_dps(PORT_D, -50);
-			sleep(i*-1);
+		if (stappen[i] < 0){
+			printf("\n%4d\n", stappen[i]);
+			BP.set_motor_dps(PORT_C, -52);
+			BP.set_motor_dps(PORT_D, -52);
+			sleep(stappen[i]*-1);
 			BP.set_motor_dps(PORT_C, 0);
 			BP.set_motor_dps(PORT_D, 0);
 			sleep(1);
+			BP.set_motor_position(PORT_A, -200);
+			sleep(1);
 			BP.set_motor_position(PORT_A, 0);
-			sleep(0.5);
-			BP.set_motor_position(PORT_A, 150);
-			sleep(0.5);
-		} else if (i > 0){
-			BP.set_motor_dps(PORT_C, 50);
-			BP.set_motor_dps(PORT_D, 50);
-			sleep(i);
+		} else if (stappen[i] >= 0){
+			
+			printf("\n%4d\n", stappen[i]);
+			BP.set_motor_dps(PORT_C, 52);
+			BP.set_motor_dps(PORT_D, 52);
+			sleep(stappen[i]);
 			BP.set_motor_dps(PORT_C, 0);
 			BP.set_motor_dps(PORT_D, 0);
 			sleep(1);
+			BP.set_motor_position(PORT_A, -200);
+			sleep(1);
 			BP.set_motor_position(PORT_A, 0);
-			sleep(0.5);
-			BP.set_motor_position(PORT_A, 150);
-			sleep(0.5);
 		}
-		sleep(1);
-		BP.set_motor_position(PORT_A, 0);
-		sleep(0.5);
-		BP.set_motor_position(PORT_A, 150);
-		sleep(0.5);
 	}
 }
 
 int main(){
+	signal(SIGINT, exit_signal_handler); // register the exit function for Ctrl+C
+	
 	vector<string> noots {"f8", "c16", "e", "b", "e8", "c", "c16", "e"};
-	move(transform(noots));
+	//vector<string> noots {"c", "c16", "g8", "a8", "e8", "f8", "c8", "f8", "g8", "c16", "g8", "a8", "e8", "f8", "c8", "f8", "g8", "c16", "g8", "a8", "e8", "f8", "c8", "f8", "g8"};
+	vector<int> temp = transform(noots);
+	move(temp);
+	
+	//BP.set_motor_dps(PORT_C, 53.5);
+	//BP.set_motor_dps(PORT_D, 53.5);
+	//sleep(1);
+	BP.set_motor_dps(PORT_C, 0);
+	BP.set_motor_dps(PORT_D, 0);
+	BP.reset_all();    // Reset everything so there are no run-away motors
+	exit(-2);
+}
+
+
+// Signal handler that will be called when Ctrl+C is pressed to stop the program
+void exit_signal_handler(int signo){
+	if(signo == SIGINT){
+		BP.reset_all();    // Reset everything so there are no run-away motors
+		exit(-2);
+	}
 }
